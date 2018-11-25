f7cd04624b4358a33a5792c3584083ebcde3e6c8
Alex L <bruinAlex@users.noreply.github.com>
2018-11-25 15:10:55 -0800

    Update v2.py

--- 311Server/v2.py
+++ 311Server/v2.py
@@ -76,9 +76,9 @@ def admin():
 # 	json_data = json.loads(response)
 # 	all_tickets = response
 	
-	json_data = get_requests()
-	print(json_data)
-	logging.debug('json data: %s', json_data
+# 	json_data = get_requests()
+# 	print(json_data)
+# 	logging.debug('json data: %s', json_data
 
 	return render_template('admin.html')
 # 	return render_template('admin.html', json_data = json_data)
